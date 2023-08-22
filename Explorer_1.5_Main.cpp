6948177fc8c774479df05545535a314edbb776f9
cRumy16 <79999770+cRumy16@users.noreply.github.com>
2023-08-22 19:10:37 +0200

    Add files via upload

---
+++ Explorer_1.5_Main.cpp
@@ -0,0 +1,309 @@
+#include <Arduino.h>
+//MOTORE DESTRA
+int DXenA = 13;     
+#define DX1 12     
+#define DX2 11
+//MOTORE SINSITRA
+int SXenB = 8;      
+#define SX3 10
+#define SX4 9
+/* CENTRALE - CENTRO_DESTRA - CENTRO_SINISTRA - LATERALE_DESTRA - LATERALE_SINISTRA */
+int Trig[5]  {40,46,44,42,48};
+int Echo[5]  {41,47,45,43,49};
+
+
+int bafDX = 14; 
+int bafSX = 15; 
+int bafCE = 50; 
+int mic = 6;
+int Luce = 16; 
+int Gas = 17; 
+
+int led_luci = 4; 
+int led_gas = 3;
+int led_suono = 5; 
+
+void SET_VELOCITA(int speed1, int speed2){
+  analogWrite(DXenA,speed1);
+  analogWrite(SXenB,speed2);
+}
+
+void FERMO(){
+  digitalWrite(DX1, LOW);         
+  digitalWrite(DX2, LOW);
+  digitalWrite(SX3, LOW);
+  digitalWrite(SX4, LOW);
+} 
+void AVANTI(){
+  digitalWrite(DX1, LOW);         
+  digitalWrite(DX2, HIGH);
+  digitalWrite(SX3, LOW);
+  digitalWrite(SX4, HIGH);
+}
+void SINISTRA(){
+  //SET_VELOCITA(150,255);
+  digitalWrite(DX1, LOW);         
+  digitalWrite(DX2, HIGH);
+  digitalWrite(SX3, HIGH);
+  digitalWrite(SX4, LOW);
+}
+void DESTRA(){
+  //SET_VELOCITA(255,150);
+  digitalWrite(DX1, HIGH);         
+  digitalWrite(DX2, LOW);
+  digitalWrite(SX3, LOW);
+  digitalWrite(SX4, HIGH);
+}
+void INDIETRO(){
+  digitalWrite(DX1, HIGH);         
+  digitalWrite(DX2, LOW);
+  digitalWrite(SX3, HIGH);
+  digitalWrite(SX4, LOW);
+}
+
+int DISTANZA(int trig, int echo){
+  int dist;
+  int duration;
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echo, HIGH);
+  dist= duration*0.034/2;
+  return dist; 
+}
+int DIST[5]; //CENTRALE - CENTRO_DESTRA - CENTRO_SINISTRA - LATERALE_DESTRA - LATERALE_SINISTRA
+void LEGGI(){
+  for(int i=0; i<5; i++){
+    DIST[i] = DISTANZA(Trig[i], Echo[i]);
+  }
+}
+void RAD(int dist1, int dist2){
+  unsigned int dist = dist1 - dist2; 
+  if(dist <= 2 && dist1 > 4 && dist1 < 7 ){ // <7
+    AVANTI();
+  }
+  else if(dist > 2 && dist < 100 && dist1 > 4 && dist1 < 7 ){ // <7
+    if(dist1 < dist2){
+    SINISTRA(); 
+    }
+    if(dist2 < dist1){
+    DESTRA();
+    }
+    //AVANTI();
+  }
+  else {
+      if(dist1 <= 3 ){
+        SINISTRA();
+        delay(30);
+        AVANTI();
+      }
+      if(dist1 >= 10 && dist2 >= 10 ){ 
+        if(dist > 2){
+            if(dist1 < dist2){
+                SINISTRA(); 
+                }
+            if(dist2 < dist1){
+                DESTRA();
+                }
+        AVANTI();
+        }else{
+          DESTRA();
+          delay(500);
+          LEGGI();
+          while( DIST[0] > 6 ) {
+            LEGGI();
+            AVANTI();
+          }
+          SINISTRA();
+          delay(600);
+          LEGGI();
+          RAD(DIST[1], DIST[3]);
+          } 
+       }
+    }
+
+} 
+
+
+
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+const unsigned long period = 5000;  //the value is a number of milliseconds
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(DXenA, OUTPUT);
+  pinMode(DX1, OUTPUT);
+  pinMode(DX2, OUTPUT);
+
+  pinMode(SXenB, OUTPUT);
+  pinMode(SX3, OUTPUT);
+  pinMode(SX4, OUTPUT);
+
+  for(int i=0;i<5;i++){
+   pinMode(Trig[i], OUTPUT);
+   pinMode(Echo[i], INPUT);
+  }
+  
+  pinMode(mic, INPUT); 
+  digitalWrite(mic, LOW);
+  pinMode(Luce, INPUT);
+  digitalWrite(Luce, LOW); 
+  pinMode(Gas, INPUT); 
+  digitalWrite(Gas, LOW); 
+
+  SET_VELOCITA(255,255);
+  for(int i=3; i<6; i++)
+    pinMode(i,OUTPUT);      // Imposto i pin dei led in OUTPUT
+
+  for(int i=3; i<6;i++)
+    digitalWrite(i, LOW);     // Imposto i pin dei led spenti}
+
+  
+  // Serial.println(" CALIBRATO"); 
+  delay(3000);
+  
+  AVANTI();
+}
+bool sinistra = false; 
+unsigned int distaDX = DIST[1] - DIST[3];
+unsigned int distaSX = DIST[2] - DIST[4];
+
+short int flag=0; 
+short int flag_gas = 0; 
+short int flag_suono = 0; 
+
+void loop() {
+  // int valoreSW = digitalRead(SwitchMode);
+  // Serial.println(valoreSW);
+  // if(valoreSW == HIGH){
+  //   Serial.println("Sto calibrando");
+  //   FERMO();
+  //   Luci();
+  //  // calibrazione(); 
+  // }
+
+// for(int i=0;i<5;i++){
+// Serial.println( "Distanza sensore "  + String(i) + String(": ") + String(DISTANZA(Trig[i],Echo[i])) ); 
+// }
+// Serial.println("______________");  
+
+//Serial.println("Valore sensorMax: " + String(sensorMax));
+
+if (digitalRead(mic) == HIGH && flag_suono==0){
+      digitalWrite(led_suono, HIGH);
+      FERMO();
+      delay(2500);
+      AVANTI(); 
+      digitalWrite(led_suono, LOW); 
+      flag_suono=1; 
+  }else if(digitalRead(mic) == LOW){
+      digitalWrite(led_suono, LOW); 
+      flag_suono = 0; 
+  }
+
+
+  if(digitalRead(Luce) == HIGH && flag==0){
+     digitalWrite(led_luci, HIGH);
+      FERMO();
+      delay(2500);
+      AVANTI();
+      digitalWrite(led_luci, LOW); 
+ 
+      flag=1; 
+  }else if(digitalRead(Luce) == LOW){
+      digitalWrite(led_luci, LOW); 
+      flag_suono = 0; 
+  }
+  
+     if(digitalRead(Gas) == HIGH && flag_gas==0){
+     digitalWrite(led_gas, HIGH);
+      FERMO();
+      delay(2500);
+      AVANTI(); 
+      digitalWrite(led_gas, LOW); 
+
+      flag_gas=1; 
+  }else if(digitalRead(Gas) == LOW){
+      digitalWrite(led_gas, LOW); 
+      flag_gas = 0; 
+  }
+  
+    
+      //SET_VELOCITA(255,255);
+LEGGI();
+if(digitalRead(bafDX) == HIGH){
+  //Serial.println("BAFFO DESTRO ATTIVO");
+  //INDIETRO();
+  while(distaDX >= 2 ){
+    SINISTRA();
+    LEGGI();
+  }
+  AVANTI();
+
+}
+if(digitalRead(bafSX) == HIGH){
+  //Serial.println("BAFFO SINISTRO ATTIVO");
+  //INDIETRO();
+  while(distaDX >= 2 ){
+    SINISTRA();
+    LEGGI();
+  }
+  AVANTI();
+  // sinistra= false; 
+  // LEGGI();
+  // if(DIST[2] > 0 && DIST[2] < 6 && sinistra == false){
+  // DESTRA();
+  // delay(1200);
+  // LEGGI();
+  // RAD(DIST[1], DIST[3]);
+  // sinistra = true; 
+}
+if(digitalRead(bafCE) == HIGH){
+  //Serial.println("BAFFO DESTRO ATTIVO");
+  while(distaDX >= 2 ){
+    SINISTRA();
+    LEGGI();
+  }
+  AVANTI();
+}
+LEGGI();
+if(DIST[2] > 0 && DIST[2] < 6 && sinistra == false){
+  DESTRA();
+  delay(1200);
+  LEGGI();
+  RAD(DIST[1], DIST[3]);
+  sinistra = true; 
+}
+  if(DIST[0] > 6 && DIST[0] < 200){
+    sinistra =true; 
+  RAD(DIST[1], DIST[3]);
+    if((DIST[1] - DIST[3]) > 10) {
+      AVANTI();
+      delay(700);
+      DESTRA();
+      delay(600);
+      AVANTI();
+      delay(650);
+    } 
+    RAD(DIST[1], DIST[3]);
+  }
+  else if(DIST[0] <= 6 && DIST[0] >0 ){
+    SINISTRA();
+    delay(600);
+    AVANTI();
+    LEGGI(); 
+    //delay(300); 
+    RAD(DIST[1], DIST[3]);
+  }
+  // else if(DIST[0] <=3){
+  //   INDIETRO();
+  //   delay(100);
+  //   RAD(DIST[1], DIST[3]);
+  // }
+  //Suono();
+  }
---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include <Arduino.h>
+
+int mic = 6;
+int count = 0; 
+
+int pulseHigh; // Integer variable to capture High time of the incoming pulse
+int pulseLow; // Integer variable to capture Low time of the incoming pulse
+float pulseTotal; // Float variable to capture Total time of the incoming pulse
+float frequency; // Calculated Frequency
+
+int GasFW = A4;
+int GasBW = A5; 
+int Luce = A3; 
+
+  
+
+int LuceMain = 11;
+int GasMain1 = 12; 
+int SuonoMain = 6; 
+int calibrazione = 7; 
+
+int sensorMin_luci = 1023;        // minimum sensor value
+int sensorMax_luci = 0;           // maximum sensor value
+
+int sensorMin_gas1 = 1023;        // minimum sensor value
+int sensorMax_gas1 = 0;           // maximum sensor value
+int sensorMin_gas2 = 1023;        // minimum sensor value
+int sensorMax_gas2 = 0;           // maximum sensor value
+
+
+
+int valore_luci = 0;
+int valore_gas1 = 0; 
+int valore_gas2 = 0; 
+
+void Luci(){
+  valore_luci = analogRead(Luce);
+  //Serial.println("Valore Luce:" + String(valore_luci));
+}
+void Gas(){
+valore_gas1 = analogRead(GasFW);
+valore_gas2 = analogRead(GasBW);
+//Serial.println("Valore gas1:" + String(valore_gas1));
+//Serial.println("Valore gas2:" + String(valore_gas2));
+//Serial.println("_________________");
+}
+void calibrazione_luci(){ 
+    // record the maximum sensor value
+    if (valore_luci > sensorMax_luci) {
+      sensorMax_luci = valore_luci;
+    }
+    // record the minimum sensor value
+    if (valore_luci < sensorMin_luci) {
+      sensorMin_luci = valore_luci;
+    }
+}
+void calibrazione_gas1(){
+  if (valore_gas1 > sensorMax_gas1) {
+      sensorMax_gas1 = valore_gas1;
+  }
+    // record the minimum sensor value
+    if (valore_gas1 < sensorMin_gas1) {
+      sensorMin_gas1 = valore_gas1;
+    }
+}
+void calibrazione_gas2(){
+  if (valore_gas2 > sensorMax_gas2) {
+      sensorMax_gas2 = valore_gas2;
+    }
+    // record the minimum sensor value
+    if (valore_gas2 < sensorMin_gas2) {
+      sensorMin_gas2 = valore_gas2;
+    }
+}
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+const unsigned long period = 5000;  //the value is a number of milliseconds
+
+void Suono(){
+int i = 0; 
+
+  // put your main code here, to run repeatedly:
+    pulseHigh = pulseIn(mic,HIGH);
+    pulseLow = pulseIn(mic,LOW);
+    pulseTotal = pulseHigh + pulseLow; // Time period of the pulse in microseconds
+    frequency=1000000/pulseTotal; // Frequency in Hertz (Hz)
+    if(i<11){
+   if(/*(frequenza > 1650 && frequenza < 4300) ||*/ (frequency > 3200 && frequency < 5000) /*|| (frequency2 > 3200 && frequency2 < 5000)*/){
+      count ++; 
+      if(count == 5){
+      //Serial.println("TROVATO SUONO");
+      digitalWrite(3, HIGH); 
+      delay(1000);
+      digitalWrite(3, LOW); 
+     } 
+     }else {
+        count =0;
+     
+    }i++;
+    }
+}
+short int flag=0; 
+short int flag_gas = 0; 
+short int flag_suono = 0; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  pinMode(mic, INPUT); 
+  pinMode(3, OUTPUT); 
+  
+    pinMode(SuonoMain, OUTPUT); 
+    digitalWrite(SuonoMain, LOW); 
+    pinMode(LuceMain, OUTPUT);
+    digitalWrite(LuceMain, LOW);
+
+    pinMode(GasMain1, OUTPUT);
+    digitalWrite(GasMain1, LOW); 
+  
+  
+    pinMode(calibrazione, INPUT_PULLUP); 
+  //digitalWrite(2, LOW); 
+   
+
+  while( millis() <3000){
+    Luci();
+      calibrazione_luci();
+    Gas();
+      calibrazione_gas1();
+      calibrazione_gas2();
+  }
+  startMillis = millis();
+}
+
+void loop() {
+    Luci();
+  if(valore_luci > sensorMax_luci + 5 && flag==0){
+      digitalWrite(LuceMain, HIGH);
+    delay(2500); 
+    flag = 1;
+  }else if(valore_luci < sensorMax_luci +5){
+    flag = 0; 
+  }
+  if(digitalRead(calibrazione) == HIGH){
+    //Serial.println("GAS"); 
+    Gas();
+    if(currentMillis - startMillis >= period && flag_gas == 0 ){
+        if((valore_gas1 > sensorMax_gas1  + 55 || valore_gas2 > sensorMax_gas2 +55 ) && flag_gas==0){
+           digitalWrite(GasMain1, HIGH);
+            //Serial.println("GAS TROVATO");
+            flag_gas = 1;
+            startMillis = millis();
+        }
+    }else if((valore_gas1 < sensorMax_gas1 ) || (currentMillis - startMillis >= period && flag_gas == 1) ){
+        flag_gas = 0;
+    }
+  }
+  Suono();
+    
+  }
+    
+    
+  
